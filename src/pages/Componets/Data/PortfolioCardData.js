df3dd8527e6c5cb86ee55427c5bedabf4878cafb
tanyaradzwa20 <tanya@uncommon.org>
2023-03-24 16:06:03 +0200

    work please

--- src/pages/Componets/Data/PortfolioCardData.js
+++ src/pages/Componets/Data/PortfolioCardData.js
@@ -2,19 +2,19 @@
 export const CardData = [
     {   
         image:  "images/website.svg" ,  
-        title: "We Are Plusvans. A waste management service that disposes of all all your unwanted garbage for you all while ensuring we keep the environment green.",
+        title: "We Are Plusvans. A waste management service that disposes of all your unwanted garbage for you all while ensuring we keep the environment green.",
         linkUrl: "/portfolio-page",
         linkText:"Visite Websites",
     },
     {   
         image:  "images/app.svg",  
-        title: "We Are Plusvans. A waste management service that disposes of all all your unwanted garbages for you all while ensuring we keep the environment green.",
+        title: "We Are Plusvans. A waste management service that disposes of all your unwanted garbages for you all while ensuring we keep the environment green.",
         linkUrl: "/portfolio-page",
         linkText:"Visite Mobile Apps"
     },
     {   
         image:  "images/dashboard.svg" ,  
-        title: "We Are Plusvans. A waste management service that disposes of all all your unwanted garbage for you all while ensuring we keep the environment green.",
+        title: "We Are Plusvans. A waste management service that disposes of all your unwanted garbage for you all while ensuring we keep the environment green.",
         linkUrl: "/portfolio-page",
         linkText:"Visite Dashboards"
     }, 
